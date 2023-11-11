1c1d0a1dfb9251b3c9138b11b1333487af5eabce
Carlos Córdova <carlos_cdo@outlook.com>
2023-11-11 12:42:15 -0500

    add camera

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,23 @@
   <div>
     <h2>Ubicación: </h2>
     <button @click="obtenerUbicacion">Obtener Ubicación</button>
-    <p v-if="isLoading">Calculando ubicación...</p>
+    <small>
+      {{ status }}
+    </small>
+    <p v-if="isLoading">Cargando ...</p>
     <p v-if="!isLoading">Latitud: {{ ubicacion.latitude }}, Longitud: {{ ubicacion.longitude }}</p>
+  </div>
+  <hr>
+  <h1>
+    Obtener una foto
+  </h1>
+  <video ref="videoElement" autoplay></video>
+  <button @click="activarCamara">Activar Cámara</button>
+  <button @click="tomarFoto">Tomar Foto</button>
+
+  <div v-if="imagenDataURL">
+    <p>Imagen:</p>
+    <img :src="imagenDataURL" alt="Imagen tomada">
   </div>
   <hr>
   <router-view/>
@@ -19,37 +34,64 @@ export default {
   name: 'App',
   data() {
     return {
+      videoElement: null,
+      imagenDataURL: null,
       isOnline: navigator.onLine,
       isLoading: true,
       ubicacion: {
         latitude: 0,
         longitude: 0,
       },
+      status: '',
     }
   },
   created() {
     this.obtenerUbicacion();
   },
   mounted() {
-
+    this.videoElement = this.$refs.videoElement;
     window.addEventListener('online', this.updateConnectionStatus);
     window.addEventListener('offline', this.updateConnectionStatus);
   },
   methods: {
-    async obtenerUbicacion() {
-      console.log("Obteniendo ubicación ....")
-      this.isLoading = true;
-      await navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log("Ubicación obtenida:", position);
-          this.ubicacion = position.coords;
-        },
-        (error) => {
-          console.error("Error al obtener la ubicación:", error);
-        }
-      );
-      this.isLoading = false;
+    obtenerUbicacion() {
+        this.status = "Obteniendo ubicación...."
+        this.isLoading = true;
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log("Ubicación obtenida:", position);
+            this.ubicacion = position.coords;
+            this.status = "Ubicación obtenida";
+            this.isLoading = false;
+          },
+          (error) => {
+            console.error("Error al obtener la ubicación:", error);
+            this.status = "Error al obtener la ubicación";
+            this.isLoading = false;
+          }
+        )
+      },
+    async activarCamara() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.videoElement.srcObject = stream;
+      } catch (error) {
+        console.error('Error al activar la cámara:', error);
+      }
     },
-  }
+
+    tomarFoto() {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      const video = this.videoElement;
+
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      this.imagenDataURL = canvas.toDataURL('image/png');
+    },
+  },
 }
 </script>
